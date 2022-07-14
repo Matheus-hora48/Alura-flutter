19baedbffe2ab1a40943387c36d14a760fca8dca
Matheus Hora <matheus11hora@gmail.com>
2022-07-13 23:39:54 -0300

    Novos

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    home: MyApp(),
-  ));
+void main() => runApp(Bytebankapp());
+
+class Bytebankapp extends StatelessWidget {
+  const Bytebankapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+        ),
+        home: FormularioTransferencia());
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class FormularioTransferencia extends StatelessWidget {
+  const FormularioTransferencia({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Realizar transferencia'),
+      ),
+    );
+  }
+}
+
+class ListaTransferencias extends StatelessWidget {
+  const ListaTransferencias({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -16,33 +38,51 @@ class MyApp extends StatelessWidget {
         title: const Text('Transferências'),
       ),
       body: Column(
-        children: const [
-          ItemTransferencia('100,0', '1020'),
+        children: [
+          ItemTransferencia(
+            transferencia: Transferencia(100.00, 859528),
+          ),
+          ItemTransferencia(
+            transferencia: Transferencia(200, 859528),
+          ),
+          ItemTransferencia(
+            transferencia: Transferencia(300, 859528),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
+      floatingActionButton: const FloatingActionButton(
+        onPressed: null,
+        child: Icon(Icons.add),
       ),
     );
   }
 }
 
 class ItemTransferencia extends StatelessWidget {
-  const ItemTransferencia(
-      {Key? key, required this.valor, required this.numeroConta})
-      : super(key: key);
-  final String valor;
-  final String numeroConta;
+  final Transferencia transferencia;
+  const ItemTransferencia({
+    Key? key,
+    required this.transferencia,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
-        leading: const Icon(Icons.monetization_on),
-        title: Text(valor),
-        subtitle: Text(numeroConta),
+        leading: const Icon(
+          Icons.monetization_on,
+          color: Colors.green,
+        ),
+        title: Text('R\$ ${transferencia.valor.toString()}'),
+        subtitle: Text(transferencia.numeroConta.toString()),
       ),
     );
   }
+}
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
 }
