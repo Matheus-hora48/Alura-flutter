c0ff18b9a93e94d5e426a1e2e2f09bd3419a3646
Matheus-hora48 <matheus11hora@gmail.com>
2022-07-15 17:10:36 -0300

    Mudanças

--- armazenamento_interno/lib/main.dart
+++ armazenamento_interno/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:armazenamento_interno/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,53 +20,6 @@ class BytebankApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSwatch()
               .copyWith(secondary: Colors.blueAccent[700])),
       home: const Dashboard(),
-    );
-  }
-}
-
-class Dashboard extends StatelessWidget {
-  const Dashboard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Dashboard'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset('images/logo.png'),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: const EdgeInsets.all(8.0),
-              height: 100,
-              width: 150,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24.0,
-                  ),
-                  Text(
-                    "contacts",
-                    style: TextStyle(color: Colors.white, fontSize: 16.0),
-                  )
-                ],
-              ),
-            ),
-          )
-        ],
-      ),
     );
   }
 }
---
+++ armazenamento_interno/lib/screens/contact_form.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ContactForm extends StatefulWidget {
+  @override
+  State<ContactForm> createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController accountNumberController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('New contact')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: const [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: 'Full name',
+              ),
+              style: TextStyle(fontSize: 24.0),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Account number',
+                ),
+                style: TextStyle(fontSize: 24.0),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Create'),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
