61b6d22c5c814fa9c972ce24d16e1e2f54b060f0
Matheus Hora <matheus11hora@gmail.com>
2022-07-19 22:06:04 -0300

    Novos

--- armazenamento_interno/lib/http/webcliente.dart
+++ armazenamento_interno/lib/http/webcliente.dart
@@ -1,4 +1,20 @@
 import 'package:http/http.dart';
+import 'package:http_interceptor/http/interceptor_contract.dart';
+import 'package:http_interceptor/http_interceptor.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print(data.toString());
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    print(data.toString());
+    return data;
+  }
+}
 
 void findAll() async {
   final Response response =
